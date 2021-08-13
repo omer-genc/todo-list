4fb013eac5766ef17459dbf8e41987615515baca
omer genc <omergnc1@gmail.com>
2021-08-14 01:21:33 +0300

    move funct not working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
-function addNewElement(){
-    let eklenecek = document.querySelector('#girilenoge').value;
+function addNewElement() {
+    let girdi = document.querySelector('#girilenoge').value;
 
-    console.log(eklenecek)
-    
-    
+
+    let yapilacaklarDOM = document.querySelector('#yapilacaklar');
+    let eklenecekHTML = `
+    <li class="list-group-item list-group-item-action list-group-item-danger">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+        ${girdi}
+        <button type="button" class="btn-close float-end" aria-label="Close"></button>
+    </li>
+    `
+
+    yapilacaklarDOM.innerHTML += eklenecekHTML
+    console.log(eklenecekHTML)
+}
+
+function move(element) {
+    let tasinacakElement = element.parentElement;
+    let yapildiDOM = document.querySelector('#yapildi');
+    yapildiDOM.append(tasinacakElement);
+
+    // if (element.checked) {
+    //     let yapildiDOM = document.querySelector('#yapildi');
+    //     yapildiDOM.append(tasinacakElement);
+    // } else {
+    //     let yapilacakDOM = document.querySelector('#yapilacaklar');
+    //     yapilacakDOM.append(tasinacakElement);
+    // }
+    console.log(tasinacakElement)
+    element.parentElement.remove();
+
+}
+
+function remove(element){
+    element.parentElement.remove();
 }
