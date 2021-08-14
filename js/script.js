49f80d92ca025a6b4080dd059d576b89cf246952
omer genc <omergnc1@gmail.com>
2021-08-14 13:15:18 +0300

    hatalar giderildi

--- js/script.js
+++ js/script.js
@@ -1,37 +1,46 @@
 function addNewElement() {
     let girdi = document.querySelector('#girilenoge').value;
+    var toastLiveExample = document.getElementById('liveToast')
+
+    if (girdi === "") {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.value = "bosdeger girilemez"
+        toast.show()
 
 
-    let yapilacaklarDOM = document.querySelector('#yapilacaklar');
-    let eklenecekHTML = `
-    <li class="list-group-item list-group-item-action list-group-item-danger">
-    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    } else {
+
+
+        let yapilacaklarDOM = document.querySelector('#yapilacaklar');
+        let eklenecekHTML = `
+        <li class="list-group-item list-group-item-action list-group-item-danger">
+        <input class="form-check-input me-1" type="checkbox" aria-label=""onchange="move(this)">
         ${girdi}
-        <button type="button" class="btn-close float-end" aria-label="Close"></button>
-    </li>
-    `
+        <button type="button" class="btn-close float-end"aria-label="Close" onclick="remove(this)"></button>
+        </li>
+        `
 
-    yapilacaklarDOM.innerHTML += eklenecekHTML
-    console.log(eklenecekHTML)
+        yapilacaklarDOM.innerHTML += eklenecekHTML
+
+    }
 }
 
 function move(element) {
     let tasinacakElement = element.parentElement;
-    let yapildiDOM = document.querySelector('#yapildi');
-    yapildiDOM.append(tasinacakElement);
 
-    // if (element.checked) {
-    //     let yapildiDOM = document.querySelector('#yapildi');
-    //     yapildiDOM.append(tasinacakElement);
-    // } else {
-    //     let yapilacakDOM = document.querySelector('#yapilacaklar');
-    //     yapilacakDOM.append(tasinacakElement);
-    // }
+
+    if (element.checked) {
+        let yapildiDOM = document.querySelector('#yapildi');
+        yapildiDOM.append(tasinacakElement);
+    } else {
+        let yapilacakDOM = document.querySelector('#yapilacaklar');
+        yapilacakDOM.append(tasinacakElement);
+    }
     console.log(tasinacakElement)
-    element.parentElement.remove();
+
 
 }
 
-function remove(element){
+function remove(element) {
     element.parentElement.remove();
 }
